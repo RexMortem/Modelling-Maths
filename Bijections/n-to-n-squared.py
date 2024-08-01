4467b99f29abe5350211ec2939f7049a4898651d
RexMortem <68436000+RexMortem@users.noreply.github.com>
2024-08-01 17:53:18 +0100

    Added comments and cleaned up code

--- Bijections/n-to-n-squared.py
+++ Bijections/n-to-n-squared.py
@@ -1,21 +1,22 @@
 # bijection between Naturals and Naturals^2 (set of all pairs of naturals)
 # Naturals include 0 here
+
 import math
 
 triangleNumber = lambda n: math.floor((n/2) * (n+1)) 
 inverseTriangleNumber = lambda n: math.floor(-1/2 + math.sqrt(2*n + 1/4)) # formula derived from completing the square on triangleNum formula
 
-print(inverseTriangleNumber(3))
-print(inverseTriangleNumber(6))
-
+# Bijection going from N to N^2
 def generatePair(n):
     layersCompleted = inverseTriangleNumber(n) # also the total 
     first = n - triangleNumber(layersCompleted)
     return (first, layersCompleted - first)
 
+# Bijection going from N^2 to N
 def generateScalar(pair):
     total = pair[0] + pair[1]
     return triangleNumber(total) + pair[0]
 
+# First 10 pairs
 for i in range(10):
-    print(generatePair(i))
+    print(i, generatePair(i), generateScalar(generatePair(i)))
